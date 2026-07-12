ec10ee2424ec9e67d17871e5e85967fcc20bb5bc
EmilynnJ <emilynnj14@gmail.com>
2026-07-12 03:47:35 -0400

    phase-1: add eslint config (backend)

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    project: "./tsconfig.json"
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier"
+  ],
+  env: {
+    node: true,
+    es2022: true
+  },
+  ignorePatterns: ["dist", "node_modules"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-misused-promises": "error",
+    "no-console": ["warn", { "allow": ["warn", "error", "info"] }]
+  }
+};
